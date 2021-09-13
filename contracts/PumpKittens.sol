cc7d85f3745d5bb8aaaafdde8ae8677dfe7a9e1d
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2021-09-14 03:29:43 +0900

    update pumpkittens

--- contracts/PumpKittens.sol
+++ contracts/PumpKittens.sol
@@ -4,49 +4,62 @@ import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAut
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Umans is ERC721PresetMinterPauserAutoId, Ownable {
-  using Counters for Counters.Counter;
-
-  uint public reservePrice = 0.0001 ether;
-  uint public currentPrice;
-  uint public previousPrice;
-  uint public addPriceRate = 400;       // 4%
-  uint public constant MAX_UMANS = 5;
-  Counters.Counter private _tokenIdTracker;
-  uint public _buyNftCount;
-
-  constructor() ERC721PresetMinterPauserAutoId("TESTUMAN", "TUMAN", "http://localhost:3000/") {
-      currentPrice = reservePrice;
-      previousPrice = 0;
-  }
-
-  function buyUman() public payable {
-    require(msg.value == currentPrice);
-    require(_tokenIdTracker.current() < MAX_UMANS);
-    _mint(_msgSender(), _tokenIdTracker.current());
-    _tokenIdTracker.increment();
+contract PumpKittens is ERC721PresetMinterPauserAutoId, Ownable {
+    using Counters for Counters.Counter;
     
-    previousPrice = currentPrice;
-    currentPrice = previousPrice + previousPrice * addPriceRate / 10000;
-  }
-  
-  function setPrice(uint _price) external onlyOwner() {
-    currentPrice = _price;
-  }
+    uint public reservePrice = 0.0001 ether;
+    uint public currentPrice;
+    uint public previousPrice;
+    uint public addPriceRate = 500;       // 5%
+    uint public constant MAX_UMANS = 50;
+    Counters.Counter private _tokenIdTracker;
+    
+    mapping(uint256 => bool) _tokenExists;
 
-  function getPrice() external view returns(uint) {
-    return currentPrice;
-  }
-  
-  function setAddPriceRate(uint _rate) external onlyOwner() {
-    addPriceRate = _rate;
-  }
+    constructor() ERC721PresetMinterPauserAutoId("Pump Kittens", "PK", "http://localhost:3000/") {
+        currentPrice = reservePrice;
+        previousPrice = 0;
+    }
+    
+    function buyUman() public payable {
+        require(msg.value == currentPrice);
+        require(_tokenIdTracker.current() <= MAX_UMANS);
+        
+        uint256 randomNumber = 0;
+        _tokenExists[randomNumber] = true;
+        
+        while (_tokenExists[randomNumber])
+        {
+            randomNumber = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, _tokenIdTracker.current()))) % 50 + 1;
+        }
+        
+        _tokenIdTracker.increment();
 
-  function getAddPriceRate() external view returns(uint) {
-    return addPriceRate;
-  }
-
-  function withdrawAll() public payable onlyOwner {
-    require(payable(msg.sender).send(address(this).balance));
-  }  
+        _mint(_msgSender(), randomNumber);
+        
+        _tokenExists[randomNumber] = true;
+        
+        previousPrice = currentPrice;
+        currentPrice = previousPrice + previousPrice * addPriceRate / 10000;
+    }
+    
+    function setPrice(uint _price) external onlyOwner() {
+        currentPrice = _price;
+    }
+    
+    function getPrice() external view returns(uint) {
+        return currentPrice;
+    }
+    
+    function setAddPriceRate(uint _rate) external onlyOwner() {
+        addPriceRate = _rate;
+    }
+    
+    function getAddPriceRate() external view returns(uint) {
+        return addPriceRate;
+    }
+    
+    function withdrawAll() public payable onlyOwner {
+        require(payable(msg.sender).send(address(this).balance));
+    }  
 }
