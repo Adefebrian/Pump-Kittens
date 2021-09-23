31c089ba9f284f3e2363d83813e3b6b59129235d
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2021-09-23 15:01:04 +0900

    test

--- client/src/components/MintMenu.vue
+++ client/src/components/MintMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <div class="text-center text-h5 font-weight-bold">{{totalSupply}} / 100 Pumpkittens Minted</div>
+    <div class="text-center text-h5 font-weight-bold">{{totalSupply}} / 4 Pumpkittens Minted</div>
     <v-row class="my-7 mx-0">
       <v-col cols="4" lg="2" md="2" sm="3" class="pa-1" v-for="(profile,idx) in profiles"
             :key="idx">
@@ -26,18 +26,18 @@ export default {
     return {
       title: "details",
       profiles: [
-        {url: require("../images/1.png")},
-        {url: require("../images/2.png")},
-        {url: require("../images/3.png")},
-        {url: require("../images/4.png")},
-        {url: require("../images/5.png")},
+        {url: require("../images/1-1.png")},
+        {url: require("../images/1-2.png")},
+        {url: require("../images/1-3.png")},
+        {url: require("../images/1-4.png")},
+ /*       {url: require("../images/5.png")},
         {url: require("../images/6.png")},
         {url: require("../images/7.png")},
         {url: require("../images/8.png")},
         {url: require("../images/9.png")},
         {url: require("../images/10.png")},
         {url: require("../images/11.png")},
-        {url: require("../images/12.png")},
+        {url: require("../images/12.png")},*/
       ],
     };
   },
